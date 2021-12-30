731775b5baf5986810a1f1f5d6c37a4d0ec8b1e9
theperiscope <1619363+theperiscope@users.noreply.github.com>
2021-12-30 11:35:48 -0600

    improve day14 part 1 using the fast pair-counting approach

--- day14/main.go
+++ day14/main.go
@@ -14,56 +14,9 @@ func part1(polymerTemplate string, rules map[string]string) {
 		pairs[k]++
 	}
 
-	p := polymerTemplate
-
-	N := 5
-	for i := 0; i < N; i++ {
-		p = step(p, rules)
-		fmt.Println(len(p), count(p), p)
-
-		pairs = step2(pairs, rules)
-		fmt.Println(pairs)
-		if len(p) == 97 {
-			pairs := map[string]int{}
-			for i := 0; i < len(p)-1; i++ {
-				k := p[i : i+2]
-				pairs[k]++
-			}
-			fmt.Println(pairs)
-		}
-		fmt.Println("----")
+	for i := 0; i < 10; i++ {
+		pairs = countPairs(pairs, rules)
 	}
-
-	counts := count(p)
-	minCount, maxCount := math.MaxInt, 0
-	fmt.Println(counts)
-	for _, v := range counts {
-		if v < minCount {
-			minCount = v
-		}
-		if v > maxCount {
-			maxCount = v
-		}
-	}
-
-	fmt.Printf("Part 1 Answer: %d - %d = %d\n", maxCount, minCount, maxCount-minCount)
-
-}
-
-func part2(polymerTemplate string, rules map[string]string) {
-	pairs := map[string]int{}
-	for i := 0; i < len(polymerTemplate)-1; i++ {
-		k := polymerTemplate[i : i+2]
-		pairs[k]++
-	}
-	fmt.Println(pairs)
-
-	N := 40
-	for i := 0; i < N; i++ {
-		pairs = step2(pairs, rules)
-	}
-
-	fmt.Println(pairs)
 
 	letters := map[string]int{}
 
@@ -81,69 +34,61 @@ func part2(polymerTemplate string, rules map[string]string) {
 		letters[k] = v / 2
 	}
 
-	fmt.Println(letters)
-
-	minCount, maxCount := math.MaxInt, 0
-	for _, v := range letters {
-		if v < minCount {
-			minCount = v
-		}
-		if v > maxCount {
-			maxCount = v
-		}
-	}
-	fmt.Printf("Part 2 Answer: %d - %d = %d\n", maxCount, minCount, maxCount-minCount)
+	min, max := minMax(letters)
+	fmt.Printf("Part 1 Answer: %d - %d = %d\n", max, min, max-min)
 }
 
-func count(polymer string) map[string]int {
-	m := map[string]int{}
-	for i := 0; i < len(polymer); i++ {
-		m[string(polymer[i])]++
-	}
-	return m
-}
-
-func step(polymer string, rules map[string]string) string {
-	p := polymer
-	insertIndexes := []int{}
-	insertValues := []string{}
-
-	for i := 0; i < len(p)-1; i++ {
-		k := p[i : i+2]
-		if _, ok := rules[k]; !ok {
-			continue
-		}
-
-		insertIndexes = append(insertIndexes, i+1)
-		insertValues = append(insertValues, rules[k])
+func part2(polymerTemplate string, rules map[string]string) {
+	pairs := map[string]int{}
+	for i := 0; i < len(polymerTemplate)-1; i++ {
+		k := polymerTemplate[i : i+2]
+		pairs[k]++
 	}
 
-	for x := 0; x < len(insertIndexes); x++ {
-		i := insertIndexes[x]
-		s := insertValues[x]
-		before, after := split(p, i+x /* each insert increases p so we need to modify index */)
-		p = before + s + after
+	for i := 0; i < 40; i++ {
+		pairs = countPairs(pairs, rules)
 	}
 
-	return p
-}
+	letters := map[string]int{}
 
-func step2(pairs map[string]int, rules map[string]string) map[string]int {
-	pairCounts := map[string]int{}
-
+	// first and last character are part of only one pair, but rest - two (e.g. in `AXB` the X is part of both AX and XB)
+	// so we need to increase first and last letter counts by 1 before we start to normalize
+	letters[string(polymerTemplate[0])]++
+	letters[string(polymerTemplate[len(polymerTemplate)-1])]++
 	for k, v := range pairs {
-		s := string(k[0]) + rules[k] + string(k[1]) // length 3
-		p1 := s[0:2]
-		p2 := s[1:3]
-		pairCounts[p1] += v
-		pairCounts[p2] += v
+		letters[string(k[0])] += v
+		letters[string(k[1])] += v
 	}
 
-	return pairCounts
+	// half the values due to pair-doubling
+	for k, v := range letters {
+		letters[k] = v / 2
+	}
+
+	min, max := minMax(letters)
+	fmt.Printf("Part 2 Answer: %d - %d = %d\n", max, min, max-min)
 }
 
-func split(s string, index int) (before string, after string) {
-	return s[:index], s[index:]
+func minMax(letters map[string]int) (min, max int) {
+	min, max = math.MaxInt, 0
+	for _, v := range letters {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return
+}
+
+func countPairs(pairs map[string]int, rules map[string]string) map[string]int {
+	pairCounts := map[string]int{}
+	for k, v := range pairs {
+		pairCounts[string(k[0])+rules[k]] += v
+		pairCounts[rules[k]+string(k[1])] += v
+	}
+	return pairCounts
 }
 
 func main() {
